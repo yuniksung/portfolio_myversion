f30c2540a877764d44cb1ddda9c700ad9b9b2f14
Joshua Sung <yuniksung@gmail.com>
2020-11-25 03:02:55 -0600

    nav error fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ window.addEventListener("scroll", function() {
 
     if(window.pageYOffset > 0) {
         menuArea.classList.add("cus-nav");
-        menuArea.classList.remove("cus-menu");
+        
     } else {
         menuArea.classList.remove("cus-nav");
         menuArea.classList.add("cus-menu");
