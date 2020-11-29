38532bd092018d1f3358fcbdf89fa328bb832ec3
Joshua Sung <yuniksung@gmail.com>
2020-11-28 22:04:25 -0600

    about me finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,10 @@ window.addEventListener("scroll", function() {
     }
 });
 
+const bg = document.getElementById('header');
+    window.addEventListener('scroll', function() {
+    bg.style.backgroundSize = 160 - +window.pageYOffset/12+'%';
+    bg.style.opacity = 1 - +window.pageYOffset/700+'';
+    console.log(bg.style.backgroundSize);
+})
+
